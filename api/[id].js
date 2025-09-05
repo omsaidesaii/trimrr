3fb3e5f6ba6bdd26315c128b4fb2101e31a955f3
omsaidesaii <omsaidesai9@gmail.com>
2025-09-05 13:04:24 +0530

    added edge function for short links

--- api/[id].js
+++ api/[id].js
@@ -1,13 +1,21 @@
-import { getUrl } from "@/db/apiUrls";
-
 export default async function handler(req, res) {
-  const { id } = req.query;
-  const url = await getUrl({ id });
+  const {
+    query: { id },
+  } = req;
+
+  // Replace this with your DB lookup (Supabase, etc.)
+  // For now, just test with a dummy redirect:
+  const db = {
+    abc123: "https://google.com",
+    xyz789: "https://github.com",
+  };
+
+  const url = db[id];
 
   if (url) {
-    res.writeHead(302, { Location: url.original_url });
+    res.writeHead(302, { Location: url });
     res.end();
   } else {
-    res.status(404).send("Not found");
+    res.status(404).send("Short link not found");
   }
 }
